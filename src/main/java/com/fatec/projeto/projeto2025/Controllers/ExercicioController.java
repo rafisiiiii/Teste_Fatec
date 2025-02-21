e6e655c92914dd11d6c6babf3bceab870d82e183
rafisiiiii <rafaella.isidoro@fatec.sp.gov.br>
2025-02-20 22:45:31 -0300

    Alterando forma de enviar parametro

--- src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
@@ -1,6 +1,9 @@
 package com.fatec.projeto.projeto2025.Controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +11,12 @@ public class ExercicioController {
 
     @GetMapping("")
     public String HelloWorld(){
-        return "Hello";
+        return"Hello";
+    }
+
+    @GetMapping("{nome}")
+    public String HelloWorld(@PathVariable Optional<String> nome){
+        return nome.isPresent() ? nome.get() : "dd";
     }
 
 }
