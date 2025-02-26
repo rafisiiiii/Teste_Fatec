41cd812a339d45adf5a1c1e52d725d1088fbeb8b
rafisiiiii <rafaella.isidoro@fatec.sp.gov.br>
2025-02-26 19:35:19 -0300

    Exercício que o prof pediu

--- src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
@@ -9,14 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExercicioController {
 
+    
+
     @GetMapping("")
-    public String HelloWorld(){
-        return"Hello";
+    public String verificaIdade(){
+        return "Informe a idade: ";
     }
 
-    @GetMapping("{nome}")
-    public String HelloWorld(@PathVariable Optional<String> nome){
-        return nome.isPresent() ? nome.get() : "dd";
+    @GetMapping("{idade}")
+    public String verificaIdade(@PathVariable Optional<String> idade){
+        idade.ifPresent(value -> System.out.println("Idade: " + value));
+        int idadeInt = Integer.parseInt(idade.get());
+            if (idadeInt >= 18) {
+                return "Idade é 18 ou mais";
+            } else {
+                return "Idade é menor que 18";
+            }
     }
-
-}
+}   
