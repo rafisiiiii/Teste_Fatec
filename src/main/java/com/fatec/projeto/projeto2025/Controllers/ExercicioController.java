90e5f199038bd931dedffa6eace1f41517aeb538
Rafaella <rafaella.isidoro@fatec.sp.gov.br>
2025-02-27 22:08:18 -0300

    Exercicio verifica par ou impar

--- src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
-
     
-
-    @GetMapping("")
+    @GetMapping("get-idade")
     public String verificaIdade(){
         return "Informe a idade: ";
     }
 
-    @GetMapping("{idade}")
+    @GetMapping("/get-idade/{idade}")
     public String verificaIdade(@PathVariable Optional<String> idade){
         idade.ifPresent(value -> System.out.println("Idade: " + value));
         int idadeInt = Integer.parseInt(idade.get());
@@ -31,5 +29,30 @@ public class ExercicioController {
             }else{
                 return "Idade inváida";
             }
+    } 
+
+    @GetMapping("")
+    public String verificaParImpar(){
+        return "Informe um número para verificar se é ímpar ou par: ";
     }
-}   
+
+    @GetMapping("/get-numero/{numero}")
+    public String verificaParImpar(@PathVariable Optional<String> numero){
+        try{
+            int numInt = Integer.parseInt(numero.get());
+            if (numInt % 2 == 0 && numInt > 0) {
+                return "Numero é par";
+            }else if (numInt % 2 != 0 && numInt > 0){
+                return "Número é impar";
+            }else if (numInt == 0){
+                return "Informe um número maior do que 0!";
+            }else{
+                return "Número inválido";
+            }
+        }catch (NumberFormatException e){
+            return "O valor informado não é um número";
+        }
+            
+    }
+    
+}
